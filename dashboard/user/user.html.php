e8be474f1fb43b7d8346d56e5e55568278e54da9
Hanyu Guo <hanyug@titan3.cs.clemson.edu>
2015-11-25 13:14:54 -0500

    updata user interface

--- dashboard/user/user.html.php
+++ dashboard/user/user.html.php
@@ -11,10 +11,10 @@
 	{echo "<br/><br/><br/>No swtich request!<br/><br/><br/>";}?>
     <?php foreach ($switchapps as $switchapp): ?>
       <form action="" method="post">
-			username: <input type="hidden" name="userid1" value="<?=$switchapp[userid1]?>"><?=$switchapp[un1]?><br>
-			Want to switch work time : <input type="hidden" name="timeid1" value="<?=$switchapp[usertime1]?>"><?=$switchapp[td1]," ",$switchapp[ts1]," - ",$switchapp[te1]?><br>
-      		with your time : <input type="hidden" name="timeid2" value="<?=$switchapp[usertime2]?>"><?=$switchapp[td2]," ",$switchapp[ts2]," - ",$switchapp[te2]?><br>
-      Reason: <input type="hidden" name="reason" value=""><?=$switchapp[reason]?><br><br/>
+			username: <input type="hidden" name="userid1" value="<?=$switchapp['userid1']?>"><?=$switchapp['un1']?><br>
+			Want to switch work time : <input type="hidden" name="timeid1" value="<?=$switchapp['usertime1']?>"><?=$switchapp['td1']," ",$switchapp['ts1']," - ",$switchapp['te1']?><br>
+      		with your time : <input type="hidden" name="timeid2" value="<?=$switchapp['usertime2']?>"><?=$switchapp['td2']," ",$switchapp['ts2']," - ",$switchapp['te2']?><br>
+      Reason: <input type="hidden" name="reason" value=""><?=$switchapp['reason']?><br><br/>
       <div style="float:left">
       <button type="submit"  name="action" class="btn btn-primary hidden-xs" value="swagree">Agree</button></div>
       <div class="text-right">
