9aa37c3a2b153ced669262d5888bd5032e65f6fe
Hanyu Guo <hanyug@titan3.cs.clemson.edu>
2015-11-25 17:33:57 -0500

    summary

--- dashboard/page-summary.html.php
+++ dashboard/page-summary.html.php
@@ -210,7 +210,7 @@
 					<div class="row profile">
 						<div class="col-lg-5 col-md-5 col-sm-5">
 							<div class="panel">
-							<?php include "./summary";?>
+							<?php include "./summary/";?>
 							</div>							
 						</div>
 						
