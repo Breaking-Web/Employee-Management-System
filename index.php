06557b477efe8513d5405078b8f14a0c5c07e564
Hanyu Guo <hanyug@titan3.cs.clemson.edu>
2015-11-25 17:11:30 -0500

    login

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ session_start();
 //$_SESSION["userid"]=0;
 unset($_SESSION["userid"]);
 unset($_SESSION["edit"]);
-unset($_SESSION["logintimes"]);
 
 
 	include '/home/jingyam/public_html/662/project/includes/db.inc.php';
