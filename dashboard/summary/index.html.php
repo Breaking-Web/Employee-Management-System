87944c0fdd710491d2d325f1a27eb12dafe1e3de
Hanyu Guo <hanyug@titan3.cs.clemson.edu>
2015-11-25 21:03:20 -0500

    summary

--- dashboard/summary/index.html.php
+++ dashboard/summary/index.html.php
@@ -7,17 +7,19 @@
 	</head>
 	<body>
 		<div class="col-lg-13 col-md-7 col-sm-7">
-			<div class="panel-body">
-				<div class="text-left bk-bg-white bk-padding-top-40 bk-padding-bottom-10">
-				<?php include 'uploadsummary.php';?>
-				</div>
-				<hr class="bk-margin-off"></hr>
-				<div class="text-left bk-bg-white bk-padding-top-20 bk-padding-bottom-10">
-				<?php include 'selfsummaries.php';?>
-				</div>
-				<hr class="bk-margin-off"></hr>
-				<div class="text-left bk-bg-white bk-padding-top-20 bk-padding-bottom-10">
-				<?php include 'groupsummaries.php';?>
+			<div class="panel">
+				<div class="panel-body">
+					<div class="text-left bk-bg-white bk-padding-top-40 bk-padding-bottom-10">
+					<?php include 'uploadsummary.php';?>
+					</div>
+					<hr class="bk-margin-off"></hr>
+					<div class="text-left bk-bg-white bk-padding-top-20 bk-padding-bottom-10">
+					<?php include 'selfsummaries.php';?>
+					</div>
+					<hr class="bk-margin-off"></hr>
+					<div class="text-left bk-bg-white bk-padding-top-20 bk-padding-bottom-10">
+					<?php include 'groupsummaries.php';?>
+					</div>
 				</div>
 			</div>
 		</div>
