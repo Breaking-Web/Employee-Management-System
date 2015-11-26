21ddc10d5792b6df1d788a67677e13e28572246d
Hanyu Guo <hanyug@titan3.cs.clemson.edu>
2015-11-25 20:49:59 -0500

    summary

--- dashboard/summary/index.php
+++ dashboard/summary/index.php
@@ -2,8 +2,6 @@
 	//session_start();
 
 	include '/home/jingyam/public_html/662/project/includes/db.inc.php';
-
-	echo "summary"."<br>";
    	if(isset($_GET['page'])){
 
 		$_SESSION["pagenum"] = $_GET['page'];
@@ -11,8 +9,5 @@
    		$_SESSION["pagenum"] = 1;
    	}
    	include 'index.html.php';
-	include 'selfsummaries.php';
-	include 'groupsummaries.php';
-
 ?>
 
