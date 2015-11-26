7c06de93f6dafe7d12231051df58bfa40abecf7b
Hanyu Guo <hanyug@titan3.cs.clemson.edu>
2015-11-25 20:57:36 -0500

    summaryu

--- dashboard/summary/index.html.php
+++ dashboard/summary/index.html.php
@@ -6,17 +6,17 @@
 		<meta charset="utf-8">
 	</head>
 	<body>
-		<div class="panel">
+		<div class="col-lg-13 col-md-7 col-sm-7">
 			<div class="panel-body">
-				<div>
+				<div class="text-left bk-bg-white bk-padding-top-40 bk-padding-bottom-10">
 				<?php include 'uploadsummary.php';?>
 				</div>
 				<hr class="bk-margin-off"></hr>
-				<div>
+				<div class="text-left bk-bg-white bk-padding-top-20 bk-padding-bottom-10">
 				<?php include 'selfsummaries.php';?>
 				</div>
 				<hr class="bk-margin-off"></hr>
-				<div>
+				<div class="text-left bk-bg-white bk-padding-top-20 bk-padding-bottom-10">
 				<?php include 'groupsummaries.php';?>
 				</div>
 			</div>
