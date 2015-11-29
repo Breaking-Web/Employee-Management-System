b8d40d0826554c900339a3b40387cd7cc6473152
guohanyu123 <hanyug@clemson.edu>
2015-11-29 13:33:17 -0500

    1129new

--- dashboard/display_evaluation.php
+++ dashboard/display_evaluation.php
@@ -1,25 +1,28 @@
 <?php
 	include '../includes/db.inc.php';
-	try{
-		$sql = 'SELECT user_info.userid,user_info.username, avg(evale) as mean FROM user_info LEFT JOIN (SELECT * FROM evaluation 
-			WHERE datediff(last_day(curdate()),time)<31 AND datediff(last_day(curdate() ),time )>0 ) AS T1 ON T1.userid = 
-			user_info.userid WHERE groupid = 1 GROUP BY user_info.userid';
-		$s = $pdo->prepare($sql);
-		$s->bindValue(':userid',$_SESSION["userid"]);	
-		$s->bindValue(':groupid',$_SESSION["groupid"]);	
-		$s->execute();
-	}
-	catch (PDOException $e){
-		echo $e;
-		// $error = 'Error select.';
-		// header("Location: ../includes/error.html.php");
-		exit(); 
-	}
+	if($_SESSION["position"]=="leader")
+	{
+		try{
+			$sql = 'SELECT user_info.userid,user_info.username, avg(evale) as mean FROM user_info LEFT JOIN (SELECT * FROM evaluation 
+				WHERE datediff(last_day(curdate()),time)<31 AND datediff(last_day(curdate() ),time )>0 ) AS T1 ON T1.userid = 
+				user_info.userid WHERE groupid = 1 GROUP BY user_info.userid';
+			$s = $pdo->prepare($sql);
+			$s->bindValue(':userid',$_SESSION["userid"]);	
+			$s->bindValue(':groupid',$_SESSION["groupid"]);	
+			$s->execute();
+		}
+		catch (PDOException $e){
+			echo $e;
+			// $error = 'Error select.';
+			// header("Location: ../includes/error.html.php");
+			exit(); 
+		}
 
-	$groupeva = array();
-	while(	$row = $s->fetch() ){	
-		if($row['mean'] == NULL) $groupeva[] = array($row['userid'],$row['username'],"N/A");
-		else $groupeva[] = array($row['userid'],$row['username'],$row['mean']);
+		$groupeva = array();
+		while(	$row = $s->fetch() ){	
+			if($row['mean'] == NULL) $groupeva[] = array($row['userid'],$row['username'],"N/A");
+			else $groupeva[] = array($row['userid'],$row['username'],$row['mean']);
+		}
+		include "evaluation_of_this_month.html.php";
 	}
-	include "evaluation_of_this_month.html.php";
 ?>
