b7ba2eb96a8a4af4e499551e0483287bea3dac29
Hanyu Guo <hanyug@titan1.cs.clemson.edu>
2015-12-01 09:59:28 -0500

    hope it's fine

--- dashboard/administ/index.php
+++ dashboard/administ/index.php
@@ -1,85 +1,320 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-<meta charset="UTF-8">
-<title>DbManager</title>
-<style type="text/css">
-*{
-    margin: 0;
-    padding: 0;
-}
-body{
-    font-size: 12px;
-    font-family: '微软雅黑';
-    color: #666;
-}
-.dbDebug{
-}
-.dbDebug .err{
-    color: #f00;
-    margin: 0 5px 0 0;
-}
-.dbDebug .ok{
-    color: #06f;
-    margin: 0 5px 0 0;
-}
-.dbDebug b{
-    color: #06f;
-    font-weight: normal;
-}
-.dbDebug .imp{
-    color: #f06;
-}
-</style>
-</head>
-<body>
-    <?php
-    require 'DbManage.class.php';
-    // session_start();
-    include '../../includes/db.inc.php'; // cwd = /home/jingyam/public_html/662/project/dashboard/administ
-    //echo "administ"."<br>";
-    switch ($_SESSION["position"])
-    {
-    case 'admin':
-    $files1 = scandir('./backup/');
-    $files1 = array_diff($files1, array(".",".."));
-    //print_r($files1);
-    include 'form.html.php';
-        // if (isset($_GET['backup']))
-    $db = new DBManage ( 'mysql1.cs.clemson.edu', 'admin662', '662admin', 'My662Project', 'utf8' );
-    if(isset($_POST['action']) and $_POST['action'] == 'backup')
-        {
-        //------1. database backup------------------------------------------------------------
-        //The values below are host address, username, password, name of database, charset respectively
-            $db->backup ('','','');
-            header('Location: .');
-        }
-        // if (isset($_GET['restore']))
-    foreach( $files1 as &$file1)
-    {
-    if(isset($_POST['action']) and $_POST['action'] == $file1)
-        {
-        //The values below are host address, username, password, name of database, charset respectively
-        //$db->backup ('','','');
-        $db->restore ( './backup/'.$file1);
-        }
-    }
-      break;  
-    default:
-        echo "you have no access to this page";
-      break;
-    }   
-    /**
-     * Created by yanue.
-     * User: yansueh
-     * Date: 12-10-27
-     * Time: 上午11:24
-     * To change this template use File | Settings | File Templates.
-     */
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+	<?php session_start(); if($_SESSION["position"] != 'admin') header("Location: ..");?>
+		<!-- Basic -->
+    	<meta charset="UTF-8" />
+
+		<title>Dashboard | Nadhif - Responsive Admin Template</title>
+		<!-- Mobile Metas -->
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		
+		<!-- Import google fonts -->
+        <link href="http://fonts.useso.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css" />
+        
+		<!-- Favicon and touch icons -->
+		<link rel="shortcut icon" href="assets/ico/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="assets/ico/apple-touch-icon.png" />
+		<link rel="apple-touch-icon" sizes="57x57" href="assets/ico/apple-touch-icon-57x57.png" />
+		<link rel="apple-touch-icon" sizes="72x72" href="assets/ico/apple-touch-icon-72x72.png" />
+		<link rel="apple-touch-icon" sizes="76x76" href="assets/ico/apple-touch-icon-76x76.png" />
+		<link rel="apple-touch-icon" sizes="114x114" href="assets/ico/apple-touch-icon-114x114.png" />
+		<link rel="apple-touch-icon" sizes="120x120" href="assets/ico/apple-touch-icon-120x120.png" />
+		<link rel="apple-touch-icon" sizes="144x144" href="assets/ico/apple-touch-icon-144x144.png" />
+		<link rel="apple-touch-icon" sizes="152x152" href="assets/ico/apple-touch-icon-152x152.png" />
+		
+	    <!-- start: CSS file-->
+		
+		<!-- Vendor CSS-->
+		<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+		<link href="assets/vendor/skycons/css/skycons.css" rel="stylesheet" />
+		<link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+		
+		<!-- Plugins CSS-->		
+		<link href="assets/plugins/bootkit/css/bootkit.css" rel="stylesheet" />	
+		<link href="assets/plugins/scrollbar/css/mCustomScrollbar.css" rel="stylesheet" />
+		<link href="assets/plugins/fullcalendar/css/fullcalendar.css" rel="stylesheet" />
+		<link href="assets/plugins/jquery-ui/css/jquery-ui-1.10.4.min.css" rel="stylesheet" />
+		<link href="assets/plugins/xcharts/css/xcharts.min.css" rel="stylesheet" />
+		<link href="assets/plugins/morris/css/morris.css" rel="stylesheet" />
+		
+		<!-- Theme CSS -->
+		<link href="assets/css/jquery.mmenu.css" rel="stylesheet" />
+		
+		<!-- Page CSS -->		
+		<link href="assets/css/style.css" rel="stylesheet" />
+		<link href="assets/css/add-ons.min.css" rel="stylesheet" />
+		
+		<!-- end: CSS file-->	
+	    
+		
+		<!-- Head Libs -->
+		<script src="assets/plugins/modernizr/js/modernizr.js"></script>
+		
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->	
+	</head>
+	
+	<body>
+	
+		<!-- Start: Header -->
+		<div class="navbar" role="navigation">
+			<div class="container-fluid container-nav">				
+				<!-- Navbar Action -->
+				<ul class="nav navbar-nav navbar-actions navbar-left">
+					<li class="visible-md visible-lg"><a href="index.php#" id="main-menu-toggle"><i class="fa fa-th-large"></i></a></li>
+					<li class="visible-xs visible-sm"><a href="index.php#" id="sidebar-menu"><i class="fa fa-navicon"></i></a></li>			
+				</ul>
+				<!-- Navbar Left -->
+				<div class="navbar-left">
+					<!-- Search Form -->					
+					<form class="search navbar-form">
+						<div class="input-group input-search">
+							<input type="text" class="form-control" name="q" id="q" placeholder="Search...">
+							<span class="input-group-btn">
+								<button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
+							</span>
+						</div>						
+					</form>
+				</div>
+				<!-- Navbar Right -->
+				<div class="navbar-right">
+					<!-- Userbox -->
+					<div class="userbox">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							<div class="profile-info">
+								<span class="name"></span>
+								<span class="role"></span>
+							</div>			
+							<i class="fa custom-caret"></i>
+						</a>
+						<div class="dropdown-menu">
+							<ul class="list-unstyled">
+								<li class="dropdown-menu-header bk-bg-white bk-margin-top-15">						
+									<div class="progress progress-xs  progress-striped active">
+										<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+											60%
+										</div>
+									</div>							
+								</li>	
+								<li>
+									<a href="page-profile.html.php"><i class="fa fa-user"></i> Profile</a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-wrench"></i> Settings</a>
+								</li>
+								<li>
+									<a href="page-invoice"><i class="fa fa-usd"></i> Payments</a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-file"></i> File</a>
+								</li>
+								<li>
+									<a href="page-login.html"><i class="fa fa-power-off"></i> Logout</a>
+								</li>
+							</ul>
+						</div>						
+					</div>
+					<!-- End Userbox -->
+				</div>
+				<!-- End Navbar Right -->
+			</div>		
+		</div>
+		<!-- End: Header -->
+		<div class="copyrights">Collect from <a href="http://www.clemson.com/" ></a></div>
+		<!-- Start: Content -->
+		<div class="container-fluid content">	
+			<div class="row">
+			
+				<!-- Sidebar -->
+				<div class="sidebar">
+					<div class="sidebar-collapse">
+						<!-- Sidebar Header Logo-->
+						<div class="sidebar-header">
+							<img src="assets/img/logo.png" class="img-responsive" alt="" />
+						</div>
+						<!-- Sidebar Menu-->
+						<div class="sidebar-menu">						
+							<nav id="menu" class="nav-main" role="navigation">
+								<ul class="nav nav-sidebar">
+									<?php include "icon/index.php"; ?>
+									<div class="divider2"></div>
+									<li class="active">
+										<a href="index.php">
+											<i class="fa fa-laptop" aria-hidden="true"></i><span>Backup and Restore</span>
+										</a>
+									</li>
+									<li >
+										<a href="userinfo.html.php">
+											<i class="fa fa-tasks" aria-hidden="true"></i><span>User Information</span>
+										</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+						<!-- End Sidebar Menu-->
+					</div>
+					<!-- Sidebar Footer-->
+					<div class="sidebar-footer">	
+						<ul class="sidebar-terms">
+							<li><a href="index.php#">Terms</a></li>
+							<li><a href="index.php#">Privacy</a></li>
+							<li><a href="index.php#">Help</a></li>
+							<li><a href="index.php#">About</a></li>
+						</ul>
+						<div class="copyright text-center">
+							<small><i class="fa fa-coffee"></i> Collect from <a href="http://www.clemson.edu/" title="clemson" target="_blank">clemson University</a></small>
+						</div>					
+					</div>
+					<!-- End Sidebar Footer-->
+				</div>
+				<!-- End Sidebar -->
+		
+				<!-- Main Page -->
+				<div class="main ">
+					<!-- Page Header -->
+					<div class="page-header">
+						<div class="pull-left">
+							<ol class="breadcrumb visible-sm visible-md visible-lg">								
+								<li><a href="index.php"><i class="icon fa fa-home"></i>Home</a></li>
+								<li class="active"><i class="fa fa-laptop"></i>Dashboard</li>
+							</ol>						
+						</div>
+						<div class="pull-right">
+							<h2>Dashboard</h2>
+						</div>					
+					</div>
+					<!-- End Page Header -->								
+					<div class="row">	
+						<div class="col-lg-8 col-md-12">
+							<div class="panel bk-widget bk-border-off">					
+									<?php include 'index~.php';?>
+							</div>
+						</div>	
+						<?php include 'member/index.php';?>
+					</div>
 
 
-?>
+					<div class="row">	
+						<div class="col-lg-8 col-md-12">
+							<div class="panel bk-widget bk-border-off">					
+									<?php include 'position.php';?>
+							</div>
+						</div>	
+						<?php include 'display_evaluation.php';?>
+						<?php include 'delete_employee.php';?>
+					</div>
 
-</body>
+					<div class="row">
+						<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+							<div class="panel bk-widget bk-webkit-fix">
+								<div class="panel-heading bk-bg-primary bk-border-info">
+									<div class="row">
+										<div class="col-xs-8 text-left bk-vcenter">
+											<h6 class="bk-margin-off">GOOGLE MAP</h6>
+										</div>							
+									</div>
+								</div>
+								<div class="panel-body bk-bg-white bk-border-white text-center bk-padding-off">
+									<div id="googleMap" class="bk-radius-top" style="height: 237px"></div>
+								</div>
+								<div class="panel-body bk-bg-white bk-border-white bk-padding-off-top bk-padding-off-bottom">
+									<div class="row">
+										<div class="col-xs-8 bk-vcenter bk-padding-20">
+											<h6 class="bk-margin-off bk-docs-font-weight-300">Clemson University jail, clemson, South Carolina</h6>
+											<h4 class="bk-fg-info bk-padding-top-10 bk-margin-off bk-docs-font-weight-300">Clemson Jail</h4>
+										</div>
+										<div class="col-xs-4 text-right bk-vcenter bk-padding-20 bk-fg-danger">
+											<i class="fa fa-map-marker fa-3x"></i>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+							<div class="panel bk-widget bk-webkit-fix">
+								<div class="panel-heading bk-bg-primary bk-border-info">
+									<div class="row">
+										<div class="col-xs-8 text-left bk-vcenter">
+											<h6 class="bk-margin-off">SWITCH REQUEST</h6>
+										</div>							
+									</div>
+								</div>
+								<div class="panel-body bk-bg-white bk-border-white text-center bk-padding-off">
+									<?php include "user/index.php"; ?>
+								</div>
+								<div class="panel-body bk-bg-white bk-border-white bk-padding-off-top bk-padding-off-bottom">
+									<div class="row">
+										<div class="col-xs-8 bk-vcenter bk-padding-20">
+											<h6 class="bk-margin-off bk-docs-font-weight-300">Clemson University jail, clemson, South Carolina</h6>
+											<h4 class="bk-fg-info bk-padding-top-10 bk-margin-off bk-docs-font-weight-300"></h4>
+										</div>
+										<div class="col-xs-4 text-right bk-vcenter bk-padding-20 bk-fg-danger">
+											
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>				
+				</div>
+				<!-- End Main Page -->			
+		
+			
+			</div>
+		</div><!--/container-->
+		
+		
+		
+		<div class="clearfix"></div>		
+		
+		
+		<!-- start: JavaScript-->
+		
+		<!-- Vendor JS-->				
+		<script src="assets/vendor/js/jquery.min.js"></script>
+		<script src="assets/vendor/js/jquery-2.1.1.min.js"></script>
+		<script src="assets/vendor/js/jquery-migrate-1.2.1.min.js"></script>
+		<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="assets/vendor/skycons/js/skycons.js"></script>		
+		
+		<!-- Plugins JS-->		
+		<script src="assets/plugins/jquery-ui/js/jquery-ui-1.10.4.min.js"></script>
+		<script src="assets/plugins/scrollbar/js/jquery.mCustomScrollbar.concat.min.js"></script>
+		<script src="assets/plugins/bootkit/js/bootkit.js"></script>
+		<script src="assets/plugins/moment/js/moment.min.js"></script>	
+		<script src="assets/plugins/fullcalendar/js/fullcalendar.min.js"></script>
+		<script src="assets/plugins/touchpunch/js/jquery.ui.touch-punch.min.js"></script>
+		<script src="assets/plugins/flot/js/jquery.flot.min.js"></script>
+		<script src="assets/plugins/flot/js/jquery.flot.pie.min.js"></script>
+		<script src="assets/plugins/flot/js/jquery.flot.resize.miSn.js"></script>
+		<script src="assets/plugins/flot/js/jquery.flot.stack.min.js"></script>
+		<script src="assets/plugins/flot/js/jquery.flot.time.min.js"></script>
+		<script src="assets/plugins/xcharts/js/xcharts.min.js"></script>
+		<script src="assets/plugins/autosize/jquery.autosize.min.js"></script>
+		<script src="assets/plugins/placeholder/js/jquery.placeholder.min.js"></script>
+		<script src="assets/plugins/datatables/js/dataTables.bootstrap.min.js"></script>
+		<script src="assets/plugins/datatables/js/jquery.dataTables.min.js"></script>
+		<script src="assets/plugins/raphael/js/raphael.min.js"></script>
+		<script src="assets/plugins/morris/js/morris.min.js"></script>
+		<script src="assets/plugins/gauge/js/gauge.min.js"></script>		
+		<script src="assets/plugins/d3/js/d3.min.js"></script>		
+		<script type="text/javascript" src="http://maps.google.com/maps/api/js"></script>
+		
+		<!-- Theme JS -->		
+		<script src="assets/js/jquery.mmenu.min.js"></script>
+		<script src="assets/js/core.min.js"></script>
+		
+		<!-- Pages JS -->
+		<script src="assets/js/pages/index.js"></script>
+		
+		<!-- end: JavaScript-->
+		
+	</body>
+	
 </html>
-
